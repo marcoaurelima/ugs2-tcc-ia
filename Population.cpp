b432788816141c79fc2853ea080245cdb54bd91d
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2022-08-24 14:28:50 -0300

    Alteração e Population

--- Population.cpp
+++ Population.cpp
@@ -9,3 +9,12 @@ Population::~Population()
 {
 
 }
+
+void Population::createInitialPopulation(const unsigned POPULATION_SIZE)
+{
+    for (size_t i = 0; i < POPULATION_SIZE; i++)
+    {
+        
+    }
+    
+}
