3d7ac403f1a918d8c6681e97b82b7d95a892ad4e
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2022-08-26 15:50:27 -0300

    correção no main

--- main.cpp
+++ main.cpp
@@ -9,9 +9,6 @@
 
 int main(int argc, char **argv) 
 {
-    selecaoestocastica(std::vector<float>{1,2,7,9}, 4, 20);
-
-    return 0;
     Population population;
     population.createInitialPopulation(3,36);
     population.show();
