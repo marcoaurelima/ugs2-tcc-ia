322102f9ba5129f0765993ed7893e523384887fc
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2022-08-26 20:06:11 -0300

    iniciando implementação da mutationInsertion

--- main.cpp
+++ main.cpp
@@ -8,10 +8,40 @@
 #include "Population.h"
 #include "types.h"
 
+void mutationInsertion(std::vector<float> chromosome)
+{
+    size_t chromosomeSize = chromosome.size();
 
+    srand((unsigned int)time(NULL));
+    unsigned index1 = 0, index2 = 0;
+    while (index1 == index2) 
+    {
+        index1 = (rand() % chromosomeSize*100)/100;
+        index2 = (rand() % chromosomeSize*100)/100;
+    }
+    
+    // Ordenar indices; index1 deve conter menor valor dos dois
+
+    if(index1 > index2)
+    {
+        unsigned temp = index1;
+        index1 = index2;
+        index2 = temp;
+    }
+
+    std::cout << index1 << " " << index2 << " " <<  "\n"; 
+
+
+
+
+}
 
 int main(int argc, char **argv) 
 {
+
+    mutationInsertion(std::vector<float>{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28});
+
+    return 0;
     Population population;
     population.createInitialPopulation(3,36);
     population.show();
