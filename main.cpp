a1f5215573dc5230e09514ecd046db06ed5ce319
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2022-09-18 13:22:58 -0300

    organizando código na main

--- main.cpp
+++ main.cpp
@@ -12,11 +12,8 @@
 #include "NN/types.h"
 #include "NN/NeuralNetwork.h"
 
-
 int main()
-{
-
-    
+{    
     NeuralNetwork network;
 
     network.setInputLayer(InputLayerInfo(2));
@@ -27,7 +24,6 @@ int main()
     Chromosome chromossome({1,2,3,4,5,6,7,8,9,10,
                             11,12,13,14,15,16,17,18,19,20,
                             21,22,23,24,25,26,27});
-
 
     network.loadDataFromChromosome(chromossome);
 
